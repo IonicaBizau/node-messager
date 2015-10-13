eb1d79fbc590ff96d99bd7eb8a79ff6478d4410e
Ionică Bizău <bizauionica@gmail.com>
2015-10-13 17:40:46 +0300

    Comments

--- example/index.js
+++ example/index.js
@@ -1,11 +1,24 @@
+// Dependencies
 var Messager = require("../lib");
 
-const FILE = __dirname + "/test";
+// Constants
+const FILE = __dirname + "/test.sock";
 
+// Create the server
 var server = Messager.Server(FILE);
+
+// Listen for "foo" messages
 server.on("foo", function (a, r, g, u, m, e, n, t, s) {
     console.log(a, r, g, u, m, e, n, t, s);
+    server.close(function (err) {
+        console.log(err || "Done.");
+    });
 });
 
-var client = Messager.Client(FILE);
-client.send("foo", "a", "r", "g", "u", "m", "e", "n", "t", "s");
+// Listen until the server is ready
+server.on("ready", function () {
+    // This code can be in another file (which is ran by another process)
+    // Create the client and send a "foo" event and some data.
+    var client = Messager.Client(FILE);
+    client.send("foo", "a", "r", "g", "u", "m", "e", "n", "t", "s");
+});
